371ad15214952e7a7396b285588b7b083daa5308
marilorequejo6 <mreqjor.genius@gmail.com>
2025-06-04 13:36:54 +0200

    Añado botones de borrado de tareas

--- static/Javascript/Funcions/PeticionPagina.js
+++ static/Javascript/Funcions/PeticionPagina.js
@@ -30,9 +30,11 @@ export async function PeticionPagina(){
                 const tareasText = await peticionTareas.json();
                 console.log("tareas: ", tareasText);
 
+                //FORMA 1: Mostrar las tareas en el HTML
                 // Ahora tengo que mostrar las tareas en el HTML
+/*                 // Ahora tengo que mostrar las tareas en el HTML
                 const refMain = document.querySelector("main");
-/*                 refMain.innerHTML = `
+                     refMain.innerHTML = `
                     <h1>Tareas</h1>
                     <ul>
                         <li>${tareasText.tareas1}</li>
@@ -42,11 +44,40 @@ export async function PeticionPagina(){
                         <li>${tareasText.tareas5}</li>
                         <li>${tareasText.tareas6}</li>
                     </ul>
-                `; */
+                `; 
                 // Recorremos el objeto de tareas y lo mostramos en el HTML
                 for (let tarea in tareasText) {
                     refMain.innerHTML += `<li>${tareasText[tarea]}</li>`;
+                } */
+
+                // FORMA 2: Mostrar las tareas en el HTML
+                let elementoDiv = document.createElement("div");
+                for (let tarea in tareasText) {
+                    let elementoP = document.createElement("p");
+                    let elementoSpan = document.createElement("span"); // Creamos un span para el texto de la tarea
+                    let elementoImg = document.createElement("img");
+
+                    // Asignamos el texto y la imagen a los elementos
+                    elementoImg.src = "../assets/Bin.png";
+                    elementoSpan.innerHTML = tareasText[tarea];
+
+                    // Asignamos estilos a los elementos
+                    elementoImg.className = "boton-eliminar";
+                    
+                    // Agregamos elementos a p
+                    elementoP.append(elementoSpan);
+                    elementoP.append(elementoImg);
+
+                    // Evento: eliminar tarea
+                    elementoImg.addEventListener("click", () => {
+                        elementoP.remove(); // Elimina solo este párrafo
+                    });
+
+                    // Agregamos todo al contenedor principal
+                    elementoDiv.append(elementoP); 
                 }
+                document.querySelector("main").append(elementoDiv); // Metemos el div en el main
+
 
                 // Botón de salir
                 salir.addEventListener("click",()=>{
